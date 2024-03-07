22dc03161aeb3b202553a55de506053c8e4dd6e5
AnggaPalguna <kadekpaguna2503@gmail.com>
2024-03-07 15:32:59 +0800

    fix navbar close

--- src/components/MenuOverlay.jsx
+++ src/components/MenuOverlay.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import NavLink from './Navlink'
+"use client";
+import React, { useEffect, useRef } from "react";
+import NavLink from "./Navlink";
 
-const MenuOverlay = ({links}) => {
+const MenuOverlay = ({ links, onClose }) => {
+  const overlayRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (overlayRef.current && !overlayRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    const handleClickInsideOverlay = (event) => {
+      if (overlayRef.current && overlayRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener("click", handleClickInsideOverlay);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("click", handleClickInsideOverlay);
+    };
+  }, [onClose]);
+
+  const handleItemClick = () => {
+    onClose();
+  };
+
   return (
-    <ul className='flex flex-col py-3 leading-[35px] items-center text-xxl mt-0 '>
-        {links.map((link, index) => (
-            <li key={index}>
-            <NavLink href={link.path} title ={link.title} />
-            </li>
-        )) }
+    <ul ref={overlayRef} className="flex flex-col py-3 leading-[35px] items-center text-xxl mt-0">
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} onClick={handleItemClick} />
+        </li>
+      ))}
     </ul>
-  ) 
-}
+  );
+};
 
-export default MenuOverlay
+export default MenuOverlay;
